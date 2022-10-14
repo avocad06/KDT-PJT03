c51486936cf13cfe30dc6658dd57af3146bca74f
avocad06 <miggo2704@gmail.com>
2022-10-14 21:17:45 +0900

    add: staticfiles_dirs

--- PJT03/settings.py
+++ PJT03/settings.py
@@ -119,7 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/' # 기본 경로
+
+STATICFILES_DIRS = [BASE_DIR/'static'] # 추가 경로
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
