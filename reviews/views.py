33fdb99a568d1784880566a350f85175044b75d8
avocad06 <miggo2704@gmail.com>
2022-10-15 11:49:31 +0900

    function: Reveiw_create

--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import PostForm
+from .models import Review
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+@login_required
 def create(request):
-    return render(request, "reviews/create.html")
+    if request.method == "POST":
+        
+        forms = PostForm(request.POST)
+        # 사용자가 입력한 값이 유효성을 통과하면 DB에 저장한다.
+        if forms.is_valid():
+            forms.save()
+            # 게시글 작성 후 제대로 작성됐는지 확인하도록 게시글 상세보기 페이지로 보낸다.
+            # return redirect(request, "reviews/detail.html", Review.objects.all()[0].pk)
+            return render(request, 'base.html') 
+    # request.method 가 GET일 경우
+    else:
+        # 빈 폼
+        forms = PostForm()
+    context = {
+        "forms" : forms,
+    }
+    return render(request, "reviews/create.html", context)
